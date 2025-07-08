2afe2c8c188d105fd347978f756be3c5a4ec7599
Igor Oliveira <igoroliveira.oliveira@gmail.com>
2025-07-08 14:01:22 -0300

    home

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,17 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <title>@yield('title', 'Meu App Laravel')</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
     <header style="background: #222; color: #fff; padding: 1rem;">
         <h1>Minha Aplicação</h1>
     </header>
 
-    <main style="padding: 2rem;">
+    <main style="padding: 1.2rem;">
         @yield('content')
     </main>
 
-    <footer style="background: #eee; padding: 1rem;">
+    <footer style="background: #222; color: #fff; padding: 1rem;">
         <p>Rodapé fixo aqui</p>
     </footer>
 </body>
